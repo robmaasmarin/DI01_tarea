bf49d504b56a3ccbec454360b66027fee38dc6b6
robmaasmarin <roberabp@gmail.com>
2024-09-25 11:20:52 +0200

    First commit

---
+++ practica_tareaDI01/src/practica_tareadi01/Practica_tareaDI01.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package practica_tareadi01;
+
+/**
+ *
+ * @author ESDPC
+ */
+public class Practica_tareaDI01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("This is the first commit");
+    }
+    
+}
